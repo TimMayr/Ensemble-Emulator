eb415f70fda5094e3a22e95c2ff8d195ad82f1c8
TimMayr <tim.mayr@gmx.at>
2025-10-19 17:05:46 +0200

    formatting

--- core/src/frontend/sdl_frontend.rs
+++ core/src/frontend/sdl_frontend.rs
@@ -1,11 +1,11 @@
 use std::cell::Ref;
 
+use sdl2::EventPump;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::render::{ScaleMode, TextureCreator, UpdateTextureError, WindowCanvas};
 use sdl2::video::WindowContext;
-use sdl2::EventPump;
 
 use crate::emulation::emu::{InputEvent, TOTAL_OUTPUT_HEIGHT, TOTAL_OUTPUT_WIDTH};
 use crate::frontend::Frontend;
